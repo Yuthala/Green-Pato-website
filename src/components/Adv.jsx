973a363eaec3b2e0f6c401f56370c6f6a5cd62ce
Yuthala <dinapolousova@gmail.com>
2024-09-21 19:39:47 +0300

    ym click counter

--- src/components/Adv.jsx
+++ src/components/Adv.jsx
@@ -4,17 +4,23 @@ import { Button } from './Button';
 import './Adv.css';
 import { Link } from 'react-router-dom';
 
+
 function Adv() {
+
+	//событие клик по кнопке "Заказать" ym
+	// const counterClick = () => {window.ym(97604546,'reachGoal','OrderClick')};
+
 	return (
 		<div className="adv-container">
 			<p className="banner"><span className="bigger-text">Предзаказ на 2025 год</span><br/>
-			Открыто бронирование на посевной материал лука и чеснока по очень выгодным ценам.</p>
+			Открыто бронирование на посевной материал лука и чеснока по выгодным ценам.</p>
 
 			<Link to="/order">
 				<Button 
 					className="btns"   
 					buttonStyle="btn--outline"
 					buttonSize="btn--large"
+					onClick={window.ym(97604546,'reachGoal','OrderClick')}
 				>
 				ЗАКАЗАТЬ
 				</Button>
