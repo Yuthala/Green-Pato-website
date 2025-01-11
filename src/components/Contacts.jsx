4a3c827c6acdb4056ddf67fb5b8bd0b6f4580b41
Yuthala <dinapolousova@gmail.com>
2025-01-11 11:12:02 +0300

    minor UI changes

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -27,8 +27,8 @@ function Contacts() {
 					<div className="contacts-mailto" itemScope itemType="https://schema.org/Organization">
 						<h4>Электронная почта</h4>
 							<Mailto className="mailto__wrapper" to="sales@greenpato.ru">
-									<label itemProp="email">sales@greenpato.ru</label>
-									<img className="contacts__img" src={email} alt="email"/>
+									<label for="email-img" role="link" itemProp="email">sales@greenpato.ru</label>
+										<img className="contacts__img" id="email-img" src={email} alt="email"/>
 							</Mailto>
 					</div>
 				</div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
 import './HeroSection.css';
 // import{ Link } from 'react-router-dom';
 import { HashLink as Link } from 'react-router-hash-link';
